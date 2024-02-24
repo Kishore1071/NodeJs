f456b87515d3f20a1b893c62f59956eeb09fca84
Kishore1071 <kishore240398@gmail.com>
2024-02-24 20:27:11 +0530

    Updated

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 import express, { json, urlencoded } from 'express'
 import { set, connect } from 'mongoose'
 import { config } from 'dotenv'
+import cors from 'cors'
 import CustomerRouter from './customers/customerRoutes.js'
 import ProductRouter from './products/productRoutes.js'
+import ServerlessHttp from 'serverless-http'
 
 const app = express()
 
 app.use(json())
 app.use(urlencoded({extended: true}))
+app.use(cors())
 
 config()
 set('strictQuery', false)
@@ -24,3 +27,5 @@ const start = async () => {
 }
 
 start()
+
+export default ServerlessHttp(app)
