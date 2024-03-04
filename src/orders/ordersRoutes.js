ddbe4aa01b2c8cd794ec1fa3b676711ee3337bea
Kishore1071 <kishore240398@gmail.com>
2024-03-04 13:40:34 +0530

    Updated

--- src/orders/ordersRoutes.js
+++ src/orders/ordersRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express'
-import { Order } from "./ordersModel.js";
+import { Order, OrderProduct } from "./ordersModel.js";
+import { Product } from '../products/productModel.js';
 
 const OrderRouter = express.Router()
 
@@ -12,13 +13,45 @@ OrderRouter.get('/', async(request, response) => {
 
 OrderRouter.post('/', async(request, response) => {
 
-    console.log(request.body)
+    const order_details = request.body[0]
+    const order_product = request.body[1]
     
-    const new_order = Order(request.body)
+    const new_order = Order(order_details)
 
     await new_order.save()
 
-    response.json(new_order)
+    let grand_total = 0
+
+    for (let x of order_product) {
+
+        const product_data = await Product.findById(x.product)
+
+        const product_amount = product_data.price * x.quantity
+
+        const product_gst = (product_amount * product_data.gst) / 100
+
+        const product_subtotal = product_amount + product_gst
+    
+        grand_total = grand_total + product_subtotal
+
+        const new_order_product = OrderProduct({
+            product: x.product,
+            order: new_order._id,
+            quantity: x.quantity,
+            amount: product_amount,
+            gst_amount: product_gst,
+            sub_total: product_subtotal
+        })
+
+        await new_order_product.save()
+
+    }
+
+    await Order.findByIdAndUpdate(new_order._id,  {
+        bill_amount: grand_total
+    })
+
+    response.json("Data Saved")
 })
 
 export default OrderRouter
